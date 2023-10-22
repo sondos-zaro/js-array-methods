53b87ef016d12ec0a76bd09ce9b71968f42729ef
sondos-zaro <sondos.zaro@gmail.com>
2023-10-22 10:06:04 +0300

    Edit countryList

--- countryList.mjs
+++ countryList.mjs
@@ -6,13 +6,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "New York",
-        "latitude": 40.7128,
-        "longitude": -74.0060
+        "coordinates": {
+          "latitude": 40.7128,
+          "longitude": -74.0060
+        }
       },
       {
         "name": "Los Angeles",
-        "latitude": 34.0522,
-        "longitude": -118.2437
+        "coordinates": {
+          "latitude": 34.0522,
+          "longitude": -118.2437
+        }
       }
     ]
   },
@@ -23,13 +27,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Toronto",
-        "latitude": 43.651070,
-        "longitude": -79.347015
+        "coordinates": {
+          "latitude": 43.651070,
+          "longitude": -79.347015
+        }
       },
       {
         "name": "Vancouver",
-        "latitude": 49.2827,
-        "longitude": -123.1207
+        "coordinates": {
+          "latitude": 49.2827,
+          "longitude": -123.1207
+        }
       }
     ]
   },
@@ -40,13 +48,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Mumbai",
-        "latitude": 19.0760,
-        "longitude": 72.8777
+        "coordinates": {
+          "latitude": 19.0760,
+          "longitude": 72.8777
+        }
       },
       {
         "name": "Delhi",
-        "latitude": 28.6139,
-        "longitude": 77.2090
+        "coordinates": {
+          "latitude": 28.6139,
+          "longitude": 77.2090
+        }
       }
     ]
   },
@@ -57,13 +69,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "London",
-        "latitude": 51.5074,
-        "longitude": -0.1278
+        "coordinates": {
+          "latitude": 51.5074,
+          "longitude": -0.1278
+        }
       },
       {
         "name": "Manchester",
-        "latitude": 53.483959,
-        "longitude": -2.244644
+        "coordinates": {
+          "latitude": 53.483959,
+          "longitude": -2.244644
+        }
       }
     ]
   },
@@ -74,13 +90,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Sydney",
-        "latitude": -33.8688,
-        "longitude": 151.2093
+        "coordinates": {
+          "latitude": -33.8688,
+          "longitude": 151.2093
+        }
       },
       {
         "name": "Melbourne",
-        "latitude": -37.8136,
-        "longitude": 144.9631
+        "coordinates": {
+          "latitude": -37.8136,
+          "longitude": 144.9631
+        }
       }
     ]
   },
@@ -91,13 +111,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Paris",
-        "latitude": 48.8566,
-        "longitude": 2.3522
+        "coordinates": {
+          "latitude": 48.8566,
+          "longitude": 2.3522
+        }
       },
       {
         "name": "Marseille",
-        "latitude": 43.2962,
-        "longitude": 5.3692
+        "coordinates": {
+          "latitude": 43.2962,
+          "longitude": 5.3692
+        }
       }
     ]
   },
@@ -108,13 +132,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Sao Paulo",
-        "latitude": -23.5505,
-        "longitude": -46.6333
+        "coordinates": {
+          "latitude": -23.5505,
+          "longitude": -46.6333
+        }
       },
       {
         "name": "Rio de Janeiro",
-        "latitude": -22.9068,
-        "longitude": -43.1729
+        "coordinates": {
+          "latitude": -22.9068,
+          "longitude": -43.1729
+        }
       }
     ]
   },
@@ -125,13 +153,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Beijing",
-        "latitude": 39.9042,
-        "longitude": 116.4074
+        "coordinates": {
+          "latitude": 39.9042,
+          "longitude": 116.4074
+        }
       },
       {
         "name": "Shanghai",
-        "latitude": 31.2304,
-        "longitude": 121.4737
+        "coordinates": {
+          "latitude": 31.2304,
+          "longitude": 121.4737
+        }
       }
     ]
   },
@@ -142,466 +174,17 @@ export let arrayOfCountries = [
     "cities": [
       {
         "name": "Moscow",
-        "latitude": 55.7558,
-        "longitude": 37.6176
+        "coordinates": {
+          "latitude": 55.7558,
+          "longitude": 37.6176
+        }
       },
       {
         "name": "Saint Petersburg",
-        "latitude": 59.9343,
-        "longitude": 30.3351
-      }
-    ]
-  },
-  {
-    "id": 10,
-    "name": "Japan",
-    "population": 126476461,
-    "cities": [
-      {
-        "name": "Tokyo",
-        "latitude": 35.682839,
-        "longitude": 139.759455
-      },
-      {
-        "name": "Osaka",
-        "latitude": 34.693738,
-        "longitude": 135.502165
-      }
-    ]
-  },
-  {
-    "id": 11,
-    "name": "South Africa",
-    "population": 59308690,
-    "cities": [
-      {
-        "name": "Johannesburg",
-        "latitude": -26.204103,
-        "longitude": 28.047305
-      },
-      {
-        "name": "Cape Town",
-        "latitude": -33.924870,
-        "longitude": 18.424055
-      }
-    ]
-  },
-  {
-    "id": 12,
-    "name": "Mexico",
-    "population": 128932753,
-    "cities": [
-      {
-        "name": "Mexico City",
-        "latitude": 19.432608,
-        "longitude": -99.133209
-      },
-      {
-        "name": "Guadalajara",
-        "latitude": 20.659698,
-        "longitude": -103.349609
-      }
-    ]
-  },{
-    "id": 13,
-    "name": "Germany",
-    "population": 83783942,
-    "cities": [
-      {
-        "name": "Berlin",
-        "latitude": 52.520008,
-        "longitude": 13.404954
-      },
-      {
-        "name": "Munich",
-        "latitude": 48.135125,
-        "longitude": 11.581981
-      }
-    ]
-  },
-  {
-    "id": 14,
-    "name": "Italy",
-    "population": 60461826,
-    "cities": [
-      {
-        "name": "Rome",
-        "latitude": 41.902782,
-        "longitude": 12.496366
-      },
-      {
-        "name": "Milan",
-        "latitude": 45.464203,
-        "longitude": 9.189982
-      }
-    ]
-  },
-  {
-    "id": 15,
-    "name": "Spain",
-    "population": 46754778,
-    "cities": [
-      {
-        "name": "Madrid",
-        "latitude": 40.416775,
-        "longitude": -3.703790
-      },
-      {
-        "name": "Barcelona",
-        "latitude": 41.385063,
-        "longitude": 2.173404
-      }
-    ]
-  },
-  {
-    "id": 16,
-    "name": "Argentina",
-    "population": 45195777,
-    "cities": [
-      {
-        "name": "Buenos Aires",
-        "latitude": -34.611765,
-        "longitude": -58.417322
-      },
-      {
-        "name": "Cordoba",
-        "latitude": -31.420083,
-        "longitude": -64.188776
-      }
-    ]
-  },
-  {
-    "id": 17,
-    "name": "Egypt",
-    "population": 102334404,
-    "cities": [
-      {
-        "name": "Cairo",
-        "latitude": 30.044420,
-        "longitude": 31.235712
-      },
-      {
-        "name": "Alexandria",
-        "latitude": 31.215645,
-        "longitude": 29.955266
-      }
-    ]
-  },
-  {
-    "id": 18,
-    "name": "Greece",
-    "population": 10423054,
-    "cities": [
-      {
-        "name": "Athens",
-        "latitude": 37.9838,
-        "longitude": 23.7275
-      },
-      {
-        "name": "Thessaloniki",
-        "latitude": 40.6401,
-        "longitude": 22.9444
-      }
-    ]
-  },
-  {
-    "id": 19,
-    "name": "Thailand",
-    "population": 69428524,
-    "cities": [
-      {
-        "name": "Bangkok",
-        "latitude": 13.7563,
-        "longitude": 100.5018
-      },
-      {
-        "name": "Chiang Mai",
-        "latitude": 18.7883,
-        "longitude": 98.9853
-      }
-    ]
-  },
-  {
-    "id": 20,
-    "name": "Kenya",
-    "population": 53771296,
-    "cities": [
-      {
-        "name": "Nairobi",
-        "latitude": -1.2921,
-        "longitude": 36.8219
-      },
-      {
-        "name": "Mombasa",
-        "latitude": -4.0435,
-        "longitude": 39.6682
-      }
-    ]
-  },
-  {
-    "id": 21,
-    "name": "South Korea",
-    "population": 51780579,
-    "cities": [
-      {
-        "name": "Seoul",
-        "latitude": 37.5665,
-        "longitude": 126.9780
-      },
-      {
-        "name": "Busan",
-        "latitude": 35.1796,
-        "longitude": 129.0756
-      }
-    ]
-  },
-  {
-    "id": 22,
-    "name": "Nigeria",
-    "population": 206139587,
-    "cities": [
-      {
-        "name": "Lagos",
-        "latitude": 6.5244,
-        "longitude": 3.3792
-      },
-      {
-        "name": "Abuja",
-        "latitude": 9.0579,
-        "longitude": 7.4951
-      }
-    ]
-  },
-  {
-    "id": 23,
-    "name": "Saudi Arabia",
-    "population": 34813871,
-    "cities": [
-      {
-        "name": "Riyadh",
-        "latitude": 24.7136,
-        "longitude": 46.6753
-      },
-      {
-        "name": "Jeddah",
-        "latitude": 21.4858,
-        "longitude": 39.1925
-      }
-    ]
-  },
-  {
-    "id": 24,
-    "name": "Turkey",
-    "population": 84339067,
-    "cities": [
-      {
-        "name": "Istanbul",
-        "latitude": 41.0082,
-        "longitude": 28.9784
-      },
-      {
-        "name": "Ankara",
-        "latitude": 39.9334,
-        "longitude": 32.8597
-      }
-    ]
-  },
-  {
-    "id": 25,
-    "name": "Indonesia",
-    "population": 273523615,
-    "cities": [
-      {
-        "name": "Jakarta",
-        "latitude": -6.2088,
-        "longitude": 106.8456
-      },
-      {
-        "name": "Surabaya",
-        "latitude": -7.2575,
-        "longitude": 112.7521
-      }
-    ]
-  },
-  {
-    "id": 26,
-    "name": "Canada",
-    "population": 37742154,
-    "cities": [
-      {
-        "name": "Toronto",
-        "latitude": 43.651070,
-        "longitude": -79.347015
-      },
-      {
-        "name": "Vancouver",
-        "latitude": 49.2827,
-        "longitude": -123.1207
-      }
-    ]
-  },
-  {
-    "id": 27,
-    "name": "New Zealand",
-    "population": 4822233,
-    "cities": [
-      {
-        "name": "Auckland",
-        "latitude": -36.8485,
-        "longitude": 174.7633
-      },
-      {
-        "name": "Wellington",
-        "latitude": -41.2865,
-        "longitude": 174.7762
-      }
-    ]
-  },
-  {
-    "id": 28,
-    "name": "Chile",
-    "population": 19116201,
-    "cities": [
-      {
-        "name": "Santiago",
-        "latitude": -33.4489,
-        "longitude": -70.6693
-      },
-      {
-        "name": "Valparaiso",
-        "latitude": -33.0472,
-        "longitude": -71.6127
-      }
-    ]
-  },
-  {
-    "id": 29,
-    "name": "Netherlands",
-    "population": 17134872,
-    "cities": [
-      {
-        "name": "Amsterdam",
-        "latitude": 52.3676,
-        "longitude": 4.9041
-      },
-      {
-        "name": "Rotterdam",
-        "latitude": 51.9225,
-        "longitude": 4.47917
-      }
-    ]
-  },
-  {
-    "id": 30,
-    "name": "Singapore",
-    "population": 5850342,
-    "cities": [
-      {
-        "name": "Singapore",
-        "latitude": 1.3521,
-        "longitude": 103.8198
-      }
-    ]
-  },
-  {
-    "id": 31,
-    "name": "Sweden",
-    "population": 10230096,
-    "cities": [
-      {
-        "name": "Stockholm",
-        "latitude": 59.3293,
-        "longitude": 18.0686
-      },
-      {
-        "name": "Gothenburg",
-        "latitude": 57.7089,
-        "longitude": 11.9746
-      }
-    ]
-  },
-  {
-    "id": 32,
-    "name": "South Africa",
-    "population": 59308690,
-    "cities": [
-      {
-        "name": "Cape Town",
-        "latitude": -33.9249,
-        "longitude": 18.4241
-      },
-      {
-        "name": "Johannesburg",
-        "latitude": -26.2041,
-        "longitude": 28.0473
-      }
-    ]
-  },
-  {
-    "id": 33,
-    "name": "Ireland",
-    "population": 4937786,
-    "cities": [
-      {
-        "name": "Dublin",
-        "latitude": 53.349805,
-        "longitude": -6.26031
-      },
-      {
-        "name": "Cork",
-        "latitude": 51.897975,
-        "longitude": -8.470610
-      }
-    ]
-  },
-  {
-    "id": 34,
-    "name": "Switzerland",
-    "population": 8654618,
-    "cities": [
-      {
-        "name": "Zurich",
-        "latitude": 47.3769,
-        "longitude": 8.5417
-      },
-      {
-        "name": "Geneva",
-        "latitude": 46.2044,
-        "longitude": 6.1432
-      }
-    ]
-  },
-  {
-    "id": 35,
-    "name": "Norway",
-    "population": 5421241,
-    "cities": [
-      {
-        "name": "Oslo",
-        "latitude": 59.9139,
-        "longitude": 10.7522
-      },
-      {
-        "name": "Bergen",
-        "latitude": 60.3913,
-        "longitude": 5.3221
-      }
-    ]
-  },
-  {
-    "id": 36,
-    "name": "Portugal",
-    "population": 10196709,
-    "cities": [
-      {
-        "name": "Lisbon",
-        "latitude": 38.7223,
-        "longitude": -9.1393
-      },
-      {
-        "name": "Porto",
-        "latitude": 41.1579,
-        "longitude": -8.6291
+        "coordinates": {
+          "latitude": 59.9343,
+          "longitude": 30.3351
+        }
       }
     ]
   }
